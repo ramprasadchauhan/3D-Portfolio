bc9200fa6804bffdda492c067ab8cb38491e0008
Ramprasad Chauhan <ramprasadchauhan7@gmail.com>
2025-04-13 00:22:40 +0530

    Hero 3D model done

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { words } from "../constants";
 import Button from "../components/Button";
+import HeroExperience from "../components/HeroModels/HeroExperience";
 
 const Hero = () => {
   return (
@@ -48,6 +49,11 @@ const Hero = () => {
           </div>
         </header>
         {/* 3D model */}
+        <figure>
+          <div className="hero-3d-layout ">
+            <HeroExperience />
+          </div>
+        </figure>
       </div>
     </section>
   );
